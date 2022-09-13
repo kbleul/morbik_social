f217691d8090c9148a93afc159ee26ea528d23f1
kiBROm <danu@localhost.localdomain>
2022-09-13 09:27:59 -0400

    post or share image and text build3

--- frontend/src/components/Postcard.js
+++ frontend/src/components/Postcard.js
@@ -3,9 +3,11 @@ import { Users } from "../dummy"
 const Postcard = ({ post , issuggestion , is_mypost }) => {
 
    //const user = Users.filter(user => post.id === user.id)[0]
+ let sec_style =  post.img === "" ? "max-h-[55vh] w-[70%] ml-[15%] my-12" : "h-[60vh] w-[70%] ml-[15%] my-12" 
 
+ 
   return (
-    <section className={ is_mypost ? "h-[60vh] mt-2 shadow-md" : "h-[60vh] w-[70%] ml-[15%] my-12"} >
+    <section className={ is_mypost ? "h-[60vh] mt-6 shadow-md" : sec_style } >
    
 
     { !is_mypost && <section>
@@ -16,16 +18,16 @@ const Postcard = ({ post , issuggestion , is_mypost }) => {
 
           {issuggestion && <button className="ml-4 text-[crimson]">+ Follow</button>}
         </div>
-          <p className="text-gray-300 text-sm">{post.date}</p>
+          <p className="text-gray-400 text-sm">{post.date}</p>
       </div>
 
-      <p className="p-2 max-h-[55vh] overflow-y-hidden hover:overflow-y-scroll">{post.desc}</p>
+      <p className={ post.img === "" ? "p-2 max-h-[30vh] overflow-y-hidden hover:overflow-y-scroll" : ""}>{post.desc}</p>
       </section>
     }
 
-   { post.img !== ""  && <img src={"/public/data/uploads/" +post.img} alt={post.desc} className="w-full h-[80%]"/>}
+   { post.img !== ""  && <img src={"/public/data/uploads/" +post.img} alt={post.desc} className="w-full h-[70%]"/>}
 
-      <div className={is_mypost ? "flex justify-between w-[80%] ml-[10%]" : "flex justify-between"}>
+      <div className={is_mypost ? "flex justify-between w-[80%] ml-[10%]" : "flex justify-between px-8 pt-4"}>
           <div className="flex items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" d="m8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385c.92 1.815 2.834 3.989 6.286 6.357c3.452-2.368 5.365-4.542 6.286-6.357c.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/></svg>
               <p className="ml-2">{post.likes.length}</p>
@@ -39,7 +41,7 @@ const Postcard = ({ post , issuggestion , is_mypost }) => {
       </div>
 
       { is_mypost &&  <div>
-          <p className="p-2">{post.desc}</p>
+          <p className="p-4">{post.desc}</p>
           <div className="flex justify-between items-center">
             <p className="px-2 text-gray-400 text-sm">{post.date}</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><circle cx="16" cy="8" r="2" fill="gray"/><circle cx="16" cy="16" r="2" fill="gray"/><circle cx="16" cy="24" r="2" fill="gray"/></svg>
